920a6262905cef5728b1d1c783858453e6356c84
Max <max3jk@gmail.com>
2019-07-25 18:06:50 +1000

    Update graph.js

--- script/graph.js
+++ script/graph.js
@@ -82,8 +82,6 @@ function Render() {
         map.children[0].children[cell.y].children[cell.x].appendChild(img);
 	}
 
-<<<<<<< HEAD
-=======
     // count - количество возвращаемых карт
     // isThis = true если выбирает текущий игрок, если false, то callback не вызывать!
     this.SelectCards = function(card, count, isThis, callback){
@@ -95,9 +93,6 @@ function Render() {
     this.StopSelect = function(){
 
     }
-
-
->>>>>>> 259a3ac33164f3294f0099d9b9e60ff574c94eff
 
 
 
