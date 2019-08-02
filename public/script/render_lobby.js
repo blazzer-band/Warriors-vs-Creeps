e5cde87a73abe3e6464815f39d8dabd8860a7c4f
Nikita <nikitabaranov200021@gmail.com>
2019-08-03 07:21:23 +1000

    make valid version

--- public/script/render_lobby.js
+++ public/script/render_lobby.js
@@ -170,16 +170,17 @@ class RenderRoom{
 		this.count = 1;
 		this.botsCount = 0;
 		this.showRoom();
-		document.getElementById("play-game").onclick = function () {
-			let db = firebase.database();
-			let roomId = db.ref("Rooms/" + room.roomKey + '/RoomId');
-			roomId.once("value", function(snapshot){
-			roomId = snapshot.val();
-		});
-			if (globalUserIdWithoutNick === roomId){
-				room.beginGame();
-			}
-		}
+		room.beginGame();
+		// document.getElementById("play-game").onclick = function () {
+		// 	let db = firebase.database();
+		// 	let roomId = db.ref("Rooms/" + room.roomKey + '/RoomId');
+		// 	roomId.once("value", function(snapshot){
+		// 	roomId = snapshot.val();
+		// });
+		// 	if (globalUserIdWithoutNick === roomId){
+		// 		room.beginGame();
+		// 	}
+		// }
 
 		document.getElementById("make-bot").onclick = function(){
 			let db = firebase.database();
